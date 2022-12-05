77fb97c5675f6430dca42cb777222182e3c3d9b8
andygnasienco <gnasadrei@gmail.com>
2022-12-04 20:21:37 -0500

    Update
    Implemented

--- agnasiencoA3/agnasiencoA3/Service1.svc.cs
+++ agnasiencoA3/agnasiencoA3/Service1.svc.cs
@@ -88,7 +88,7 @@ namespace agnasiencoA3
             string Message;
             SqlConnection con = new SqlConnection("Server=DESKTOP-A6AL2TJ\\SQLEXPRESS2;Database=vegdb;User Id=sa;Password=admin123;");
             con.Open();
-            SqlCommand cmd = new SqlCommand("insert into vegetable(veg_id, name) values(@veg_id, @name)", con);
+            SqlCommand cmd = new SqlCommand("update vegetable set name = @name where veg_id = @veg_id", con);
             cmd.Parameters.AddWithValue("@veg_id", vegetableDetails.Veg_ID);
             cmd.Parameters.AddWithValue("@name", vegetableDetails.Name);
             int result = cmd.ExecuteNonQuery();
